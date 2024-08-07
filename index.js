c1dd88ae4d621769f8836fc70d29c250f004ab66
Spectrator <93487432+MyagkiyBogdan@users.noreply.github.com>
2024-08-07 12:29:45 +0300

    updated server 3

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3065;
+
+app.use(express.json());
+
+app.post("/log", (req, res) => {
+  const { message } = req.body;
+  if (message) {
+    console.log(message);
+    res.status(200).send("Message logged server 3", message);
+  } else {
+    res.status(400).send("No message provided");
+  }
+});
+
+app.get("/message", (req, res) => {
+  res.send("Hello, this is your message from server: 3");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
